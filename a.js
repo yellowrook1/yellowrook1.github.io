3c9e8a6b770d8b6002e9dec4cc9b558b30c8eb7a
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2021-12-08 15:02:47 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,20 +1,7 @@
 fetch("https://www.chess.com/settings/connected-accounts", {
   "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-    "cache-control": "no-cache",
     "content-type": "application/x-www-form-urlencoded",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
   },
-  "referrer": "https://www.chess.com/settings/connected-accounts",
   "referrerPolicy": "strict-origin-when-cross-origin",
   "body": "form%5Bconnect_google%5D=&form%5B_token%5D=kakaOOc1wx1zr1uS2aX27YXzG-k3vj4TDgAP3U95KLY",
   "method": "POST",
