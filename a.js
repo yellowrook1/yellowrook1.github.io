cdc93d03524bc48893bf94659725aa6194814300
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2021-12-13 20:14:07 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,7 @@
 fetch("https://www.chess.com/callback/messages/list/inbox").then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {console.log(data);
+  for (var i = 0; i < data.length; i++){
+      fetch("https://www.chess.com/callback/messages/show/"+data[i].user.username).then(response => response.json())
+  .then(data => {for (var i = 0; i < data.length; i++){console.log("Time: " + data[i].createDate + " From: " + data[i].from_user.username + " To: " + data[i].to_user.username + " Message: " + data[i].message)}})
+  }
+})
