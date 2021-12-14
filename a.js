dbdecbc3c441355429e401ef13e38ff2cc793b1e
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2021-12-13 19:44:22 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,9 @@
-fetch("https://www.chess.com/callback/messages/list/inbox"
-).then(response => response.json())
+fetch("https://www.chess.com/callback/messages/list/inbox", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+  },
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+}).then(response => response.json())
   .then(data => console.log(data));
