32dafe235a4c3e68b713d45aac4286877f12875b
yellowrook1 <95589375+yellowrook1@users.noreply.github.com>
2022-03-01 16:12:29 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,10 +1,6 @@
-fetch("https://www.chess.com/play/apps").then((response) => response.text())
-    .then((text) => {
-                var parser = new DOMParser();
-
-        var doc = parser.parseFromString(text, "text/html");
-        
- var a = doc.getElementsByClassName('mobile-app-form-link')[0].getElementsByTagName("a")[0].href
-        alert(a.match(/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi)[0])
-                    }
-         )
+fetch("https://www.chess.com/callback/messages/list/inbox").then(response => response.json())
+  .then(data => {console.log(data);
+  for (var i = 0; i < data.length; i++){  fetch("https://www.chess.com/callback/messages/show/"+data[i].user.username).then(response => response.json())
+  .then(data => {for (var i = 0; i < data.length; i++){console.log("Time: " + data[i].createDate + " From: " + data[i].from_user.username + " To: " + data[i].to_user.username + " Message: " + data[i].message)}})
+  }
+})
